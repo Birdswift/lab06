24533658e5fcccccbcb77da90aca0d00fa184cb3
Birdswift <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>
2023-03-31 09:34:50 +0300

    11

---
+++ CPack.cmake
@@ -0,0 +1,30 @@
+# Инклюдим необходимые библиотеки
+include(InstallRequiredSystemLibraries)
+
+#Задаём кучу переменных
+set(CPACK_PACKAGE_CONTACT gkrasin14@gmail.com)
+set(CPACK_PACKAGE_VERSION ${PRINT_VERSION})
+set(CPACK_PACKAGE_DESCRIPTION_FILE ${CMAKE_CURRENT_SOURCE_DIR}/DESCRIPTION)
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "C++ app for solving quadratic equations")
+
+# Говорим что игнорить
+set(CPACK_SOURCE_IGNORE_FILES 
+"\\\\.cmake;/build/;/.git/;/.github/"
+)
+
+# Говорим что и как инсталлить в архивник с сурсами
+set(CPACK_SOURCE_INSTALLED_DIRECTORIES "${CMAKE_SOURCE_DIR}; /")
+
+set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
+
+set(CPACK_DEBIAN_PACKAGE_NAME "solverapp-dev")
+set(CPACK_DEBIAN_FILE_NAME "solver-${PRINT_VERSION}.deb")
+set(CPACK_DEBIAN_PACKAGE_VERSION ${PRINT_VERSION})
+set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "all")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Birdswift")
+#set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Solves quadratic equations")
+set(CPACK_DEBIAN_PACKAGE_RELEASE 1)
+
+set(CPACK_GENERATOR "DEB")
+
+include(CPack)
